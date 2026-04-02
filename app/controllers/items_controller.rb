39589b77f318456929d857bb7593c2eb619e5863
hypocrite-hero <kqiantian77@gmail.com>
2026-04-02 21:17:29 +0900

    出品した商品のデータを保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create]
+
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+  end
 end
