70ce7b4a0ee763e6e8e708716f1b8d2e7b1d9bfd
hypocrite-hero <kqiantian77@gmail.com>
2026-04-02 18:16:45 +0900

    バリデーションの記述（AI修正前）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,17 @@
 class Item < ApplicationRecord
   has_one_attached :image
-
   belongs_to :user
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+
+  validates :category_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_status_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "can't be blank"}
 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
