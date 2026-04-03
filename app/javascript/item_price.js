88173974ae4bd09192f9ad1693e384e2fc618add
hypocrite-hero <kqiantian77@gmail.com>
2026-04-03 17:12:50 +0900

    JavaScriptの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,11 +4,14 @@ const price = () => {
  if (!priceInput) return;
  priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  const tax = addTaxDom.innerHTML;
-  const profitDom = document.getElementById("profit");
-  profitDom.innerHTML = Math.floor(inputValue - tax);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = Math.floor(inputValue - tax);
+
+    addTaxDom.innerHTML = tax.toLocaleString();
+    profitDom.innerHTML = profit.toLocaleString();
  });
 
 };
