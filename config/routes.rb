c88c247a1df81678c745d217bd26c1f94de24591
hypocrite-hero <kqiantian77@gmail.com>
2026-04-06 21:28:06 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update ]
+  resources :items
   
   get "up" => "rails/health#show", as: :rails_health_check
 end
