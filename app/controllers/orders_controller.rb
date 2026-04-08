f11ced6fe38bae5b26c3ea903dcedbc935eaf118
hypocrite-hero <kqiantian77@gmail.com>
2026-04-08 22:52:57 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,21 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :prevent_url_entry, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
-    # 手順2-6: Formオブジェクトの空のインスタンスを生成
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    
-    # バリデーションをチェック
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,6 +23,30 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
+      user_id: current_user.id, 
+      item_id: params[:item_id], 
+      token: params[:token]
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.item_price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def prevent_url_entry
+    # 条件：出品者である、または、売却済みである
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,8 @@
 class OrderAddress
   include ActiveModel::Model
 
-  # 1-3. 保存したい全てのカラムを定義（tokenも含める）
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
-  # 1-4. バリデーションの処理を書く
   with_options presence: true do
     validates :user_id
     validates :item_id
@@ -18,10 +16,20 @@ class OrderAddress
 
   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
 
-  # 1-5. データをテーブルに保存する処理を書く
   def save
-    # 各テーブルにデータを保存する
+    
+    # 1. orderテーブルに保存
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    
+    # 2. addressテーブルに保存
+    Address.create(
+      postal_code: postal_code, 
+      prefecture_id: prefecture_id, 
+      city: city, 
+      house_number: house_number, 
+      building_name: building_name, 
+      phone_number: phone_number, 
+      order_id: order.id
+    )
   end
 end
