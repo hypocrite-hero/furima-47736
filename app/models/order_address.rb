0b31aa1695c8288fe8056ddb857b9199b2d0d82f
hypocrite-hero <kqiantian77@gmail.com>
2026-04-08 15:43:41 +0900

    ix: Rails 7.1でのテスト実行時におけるMysql2::Error(Fiber)を解消
    Rails 7.1以降で導入された非同期クエリ実行とFiberの仕組みが、
    WSL2上のmysql2接続と競合し「This connection is in use by...」エラーが発生。
    これを解消するために以下の設定変更を実施：
    
    - config/environments/test.rb:
      async_query_executor を nil に設定し非同期実行を無効化。
    - spec/rails_helper.rb:
      use_transactional_fixtures を false に変更し接続の排他制御を回避。
    - config/database.yml:
      test環境に prepared_statements: false を追加。
    - OrderAddressモデル:
      バリデーションの presence と allow_blank を整理しメッセージの競合を解消。

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,13 +8,15 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_number
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
     validates :token
   end
+
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
 
   # 1-5. データをテーブルに保存する処理を書く
   def save
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    # 変数として定義し、インデントを揃える
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+  end
+
+  describe '商品購入機能' do
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @order_address.building_name = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+    end
+  end
+end
