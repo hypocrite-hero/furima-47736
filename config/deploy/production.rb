6a3a940c39c3de8419563915d9b5a29706dd5bc4
hypocrite-hero <kqiantian77@gmail.com>
2026-04-22 02:05:06 +0900

    間違えを修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '13.112.252.106', user: 'ec2-user', roles: %w{app db web}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,3 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
 
-server '13.112.252.106', user: 'ec2-user', roles: %w{app db web}
-
